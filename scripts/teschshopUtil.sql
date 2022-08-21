32fd505cf96cc1265489755dfc4d76de41385e6c
cpuspak <chkrhn123@gmail.com>
2022-08-21 20:25:05 +0530

    sql script to setup db

---
+++ scripts/teschshopUtil.sql
@@ -0,0 +1,32 @@
+use techshop;
+
+
+insert into product_categories values 
+(1, "sample description 1", "product category 1");
+
+insert into product_categories values 
+(2, "sample description 2", "product category 2");
+
+insert into product values 
+(1, 4, "sample product description 1", "image", "sample product 1", 1500, 1);
+insert into product values 
+(2, 5, "sample product description 2", "image", "sample product 2", 1600, 1);
+
+select * from product
+
+use techshop;
+insert into product_categories values
+(1, "Laptops", "Laptops");
+
+insert into product_categories values
+(2, "Central Processing Units", "CPUs");
+
+insert into product_categories values
+(3, "Graphics Processing Units", "GPUs");
+
+insert into product_categories values
+(4, "Motherboards", "Motherboards")
+
+select * from customer
+select * from cart_item
+select * from product
